01e9d3ecea6db92c79c075e2f351edca80d3a028
Sebastian Stiller <wingen@gmail.com>
2017-07-05 21:29:38 +0200

    Added explicit namespace for 'cout' calls.

--- src/camera/CameraController.cpp
+++ src/camera/CameraController.cpp
@@ -9,7 +9,7 @@
 #include "CameraSimulator.h"
 
 CameraController::CameraController(bool simulator) {
-	cout << "CameraController() constructor" << endl;
+	std::cout << "CameraController() constructor" << endl;
 
 	if (simulator) {
 		m_camera.reset(new CameraSimulator());
@@ -17,21 +17,21 @@ CameraController::CameraController(bool simulator) {
 }
 
 CameraController::~CameraController() {
-	cout << "CameraController() destructor" << endl;
+	std::cout << "CameraController() destructor" << endl;
 	if (m_camera) {
 		m_camera.reset();
 	}
 }
 
 void CameraController::start() {
-	cout << "CameraController - start()" << endl;
+	std::cout << "CameraController - start()" << endl;
 	if (m_camera) {
 		m_camera->startGrab();
 	}
 }
 
 void CameraController::stop() {
-	cout << "CameraController - stop()" << endl;
+	std::cout << "CameraController - stop()" << endl;
 	if (m_camera) {
 		m_camera->stopGrab();
 	}
